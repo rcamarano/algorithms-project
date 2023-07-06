4573709a85166ba93be137ef5f92f5994a2a64c2
rcamarano <rogeriocamarano@gmail.com>
2023-07-06 17:39:51 -0300

    req 1 done and passing on local tests

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    # raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count = 0
+    for time_start, time_end in permanence_period:
+        if not isinstance(time_start, int) or not isinstance(time_end, int):
+            return None
+        if time_start <= target_time <= time_end:
+            count += 1
+        # if not isinstance(time_start, int) or not isinstance(time_end, int):
+        #     return None
+        # if time_start <= target_time <= time_end:
+        #     count += 1
+    return count
