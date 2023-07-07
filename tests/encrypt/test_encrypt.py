8305783741453854c88e51e4a76addacff1423e0
rcamarano <rogeriocamarano@gmail.com>
2023-07-07 18:07:01 -0300

    req 2 passing on local tests

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,28 +3,28 @@
 import pytest
 from challenges.challenge_encrypt_message import encrypt_message
 
-message = "batatinha"
-odd_key = 5
-even_key = 4
-invalid_message = 10
-invalid_key = "4"
+def test_encrypt_message():
+    # Testes com chave ímpar
+    assert encrypt_message("batatinha", 5) == "tatab_ahni"
+    assert encrypt_message("batatinha", 3) == "tab_ahnita"
+    assert encrypt_message("batatinha", 1) == "b_ahnitata"
 
+    # Testes com chave par
+    assert encrypt_message("batatinha", 4) == "ahnit_atab"
+    assert encrypt_message("batatinha", 2) == "ahnitat_ab"
+    assert encrypt_message("batatinha", 6) == "ahn_itatab"
 
-def test_correct_encrypt_odd():
-    encrypt = encrypt_message(message, odd_key)
-    assert encrypt == 'tatab_ahni'
+    # Testes com chave fora dos limites
+    assert encrypt_message("batatinha", 0) == "ahnitatab"
+    assert encrypt_message("batatinha", 10) == "ahnitatab"
 
+    # Testes com tipos inválidos
+    with pytest.raises(TypeError):
+        encrypt_message(10, 4)
+        encrypt_message("batatinha", "4")
+        encrypt_message("batatinha", 4.5)
+        encrypt_message("batatinha", True)
+        encrypt_message("batatinha", False)
 
-def test_correct_encrypt_even():
-    encrypt = encrypt_message(message, even_key)
-    assert encrypt == 'atab_anhit'
-
-
-def test_message_error():
-    with pytest.raises(TypeError, match="invalid message type"):
-        encrypt_message(invalid_message, even_key)
-
-
-def test_invalid_key():
-    with pytest.raises(TypeError, match="invalid key type"):
-        encrypt_message(message, invalid_key)
+    # Teste com string vazia
+    assert encrypt_message("", 4) == ""
