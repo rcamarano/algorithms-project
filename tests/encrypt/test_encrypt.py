9e5de8b917f8dbed5abf4200ea289caa7b36e349
rcamarano <rogeriocamarano@gmail.com>
2023-07-07 13:55:47 -0300

    req 2 trying one logic path

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,30 @@
 # from challenges.challenge_encrypt_message import encrypt_message
 
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
-def test_encrypt_message():
-    pass
+message = "batatinha"
+odd_key = 5
+even_key = 4
+invalid_message = 10
+invalid_key = "4"
+
+
+def test_correct_encrypt_odd():
+    encrypt = encrypt_message(message, odd_key)
+    assert encrypt == 'tatab_ahni'
+
+
+def test_correct_encrypt_even():
+    encrypt = encrypt_message(message, even_key)
+    assert encrypt == 'atab_anhit'
+
+
+def test_message_error():
+    with pytest.raises(TypeError, match="invalid message type"):
+        encrypt_message(invalid_message, even_key)
+
+
+def test_invalid_key():
+    with pytest.raises(TypeError, match="invalid key type"):
+        encrypt_message(message, invalid_key)
