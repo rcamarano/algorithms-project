1dcd5f33875f2b085183866eb8fb84c52a80ef3b
rcamarano <rogeriocamarano@gmail.com>
2023-07-14 14:57:26 -0300

    req 6, palindrome iterative challenge structured

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    min_i = 0
+    max_i = len(word) - 1
+
+    while min_i < max_i:
+        if word[min_i] != word[max_i]:
+            return False
+        min_i += 1
+        max_i -= 1
+
+    return True
