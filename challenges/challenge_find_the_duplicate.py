691bc93699b632334b5237a82a6649f3d7113f78
rcamarano <rogeriocamarano@gmail.com>
2023-07-14 14:50:07 -0300

    req 5, find the duplicate structured

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,20 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        not nums
+        or isinstance(nums, str)
+        or len(nums) == 1
+        or len(set(nums)) == len(nums)
+    ):
+        return False
+    if min(nums) < 0:
+        return False
+
+    nums.sort()
+
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return num
+        seen.add(num)
+    return False
